c6433a265fbbf36b59eb1be7a3f8af2e75919154
urmasa-tar <nik.goltsev.97@bk.ru>
2024-03-11 13:55:02 +0700

    auto_save

--- operations_analyzer/BankTransactionAnalyzerSimple.java
+++ operations_analyzer/BankTransactionAnalyzerSimple.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -23,6 +26,24 @@ public class BankTransactionAnalyzerSimple {
             total += amount;
         }
         System.out.println("The total for all transactions is " + total);
+
+    }
+
+    public static void sum_Jan(String... args){
+
+        final Path path = Paths.get(RESOURCES + args[0]);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        for(final String line: lines) {
+            final String[] columns = line.split (",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if(date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+            System.out.println("The total for all transactions in January is " + total);
+        }
 
     }
 
