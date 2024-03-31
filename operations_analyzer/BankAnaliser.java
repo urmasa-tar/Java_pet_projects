d673f64d148bee3128cf2e3f40ebfff0092c622d
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-03-31 11:20:17 +0300

    auto_save

--- operations_analyzer/BankAnaliser.java
+++ operations_analyzer/BankAnaliser.java
@@ -1,12 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class BankAnaliser{
 
-    public static final String Resourse = "./data/"
+    public static final String Resourse = "./data/";
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException{
+
+        final Path path = Paths.get(Resourse + args[0]);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        for (final String line: lines){
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("The total number is " + total);
+
     }
 
 }
