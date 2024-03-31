72c003aea13a087e1fe5ca3b71fe53d03c9b5aea
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-03-31 11:43:33 +0300

    auto_save

---
+++ operations_analyzer/My_Own_Analyser.java
@@ -0,0 +1,20 @@
+
+
+
+public class My_Own_Analyser {
+
+    public static void main(final String ... args){
+
+
+    }
+
+    public static void vargs(final String... strings) {
+        System.out.println(strings);
+    }
+
+    public static void arrs(final String[] strings) {
+        System.out.println(strings);
+    }
+
+
+}
