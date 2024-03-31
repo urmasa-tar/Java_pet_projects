33a7295a0df33dd7518d0ad4a9fce8984c0e922c
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-03-31 10:37:14 +0300

    auto_save

---
+++ operations_analyzer/BankAnaliser.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class BankAnaliser{
+
+    public static final String Resourse = "./data/"
+
+    public static void main(String[] args){
+        
+    }
+
+}
