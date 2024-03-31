4568117015c465ebdc3bde93fa1440b58dec2e5a
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-03-31 12:30:08 +0300

    With_arrs_args_and_trycatch_examples

--- operations_analyzer/My_Own_Analyser.java
+++ operations_analyzer/My_Own_Analyser.java
@@ -1,13 +1,38 @@
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class My_Own_Analyser {
 
-    public static void main(final String ... args){
+    // arrs example in Function main -> (String[] args)
+    public static void main(final String[] args){
+
+        try{
+            System.out.println(args[0]);
+        }catch (ArrayIndexOutOfBoundsException e){
+
+        } finally { // Example for tey/catch/finnaly and
+            int ln = args.length;
+            if( ln > 0){
+                int[] arr = new int[ln];
+                for(int i = 0; i < ln; i++){
+                    arr[i] = i;
+                }
+
+                System.out.println(vargs_example(arr));
+            }
+
+
+        }
 
 
     }
 
+    /*
     public static void vargs(final String... strings) {
         System.out.println(strings);
     }
@@ -15,6 +40,16 @@ public class My_Own_Analyser {
     public static void arrs(final String[] strings) {
         System.out.println(strings);
     }
+    */
 
+    public static int vargs_example(int ... n){
+        int sum = 0;
+
+        for(int i : n){
+            sum += i;
+        }
+
+        return  sum;
+    }
 
 }
